9b2ca04cdfb8e13b3b3307cdabeb543f471dcfca
Ales717 <prosenjak.ales77@gmail.com>
2024-04-05 13:24:33 +0200

    Users & Auth

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import Logging from 'library/Logging'
 
 import { AppModule } from './modules/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  })
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  app.use(cookieParser())
 
+  app.use('/files', express.static('files'))
   const PORT = process.env.PORT || 8080
   await app.listen(PORT)
+
+  Logging.log(`App is listening on: ${await app.getUrl()}`)
 }
 bootstrap()
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,27 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { confingValidationSchema } from 'config/schema.config'
+import { DatabaseModule } from './database/database.module'
+import { LoggerMiddleware } from 'middleware/logger.middleware'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.STAGE}`],
+      validationSchema: confingValidationSchema,
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
